a1c2b6fdade1f2e3dfba72d219e9d0815f43de58
Gerardo Lupa <gerardo.lupa@gmail.com>
2018-03-25 13:48:55 -0300

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  AgenceCalendar
+//
+//  Created by Gerardo Lupa on 24-03-18.
+//  Copyright © 2018 Gerardo Lupa. All rights reserved.
+//
+
+import UIKit
+import EventKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
